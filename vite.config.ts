632798af5df2fdd0af981beb8ba0ce17a024b18f
bmouseProductions <137824606+bmouseProductions@users.noreply.github.com>
2024-12-04 14:21:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Alias para a pasta src
-      '~': path.resolve(__dirname, 'public'), // Alias para a pasta public
-    },
-  },
+  base: '/'
 })
