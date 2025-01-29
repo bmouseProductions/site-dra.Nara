275f187e7829f1210b380cd7dc62c67cdc2d0ffc
beno1234 <benolopesdias@gmail.com>
2025-01-29 13:38:08 -0300

    speed

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react"; // Importação do Speed Insights
 
-import {BrowserRouter as Router} from "react-router-dom";
+import App from "./app";
 
-import App from './app';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <App />
+      <SpeedInsights /> {/* Adicionando o Speed Insights */}
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
