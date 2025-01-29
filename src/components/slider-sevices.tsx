176165b3a4918a0090bd3cce5f69b99d314c51e8
beno1234 <benolopesdias@gmail.com>
2025-01-29 14:24:25 -0300

    testar

--- src/components/slider-sevices.tsx
+++ src/components/slider-sevices.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
-
 import "swiper/css";
 import "swiper/css/pagination";
 import ModalServices from "../components/modal-services";
@@ -19,19 +18,16 @@ interface SlideServicesProps {
 }
 
 export default function SlideServices({ services, light }: SlideServicesProps) {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [selectedService, setSelectedService] =
     useState<ServiceInterface | null>(null);
 
-  const openModal = (service: ServiceInterface) => {
+  const openModal = useCallback((service: ServiceInterface) => {
     setSelectedService(service);
-    setIsModalOpen(true);
-  };
+  }, []);
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const closeModal = useCallback(() => {
     setSelectedService(null);
-  };
+  }, []);
 
   return (
     <>
@@ -40,36 +36,34 @@ export default function SlideServices({ services, light }: SlideServicesProps) {
         slidesPerView={1.2}
         slidesPerGroupSkip={1}
         pagination={{ clickable: true }}
-        loop={true}
+        loop
         breakpoints={{
           768: { slidesPerView: 2.1 },
           1024: { slidesPerView: 2.2 },
           1280: { slidesPerView: 3.2 },
         }}
         modules={[Autoplay, Pagination]}
-        className="!pb-10 !pl-5 md:!pl-10 lg!:pl-14 xl:!pl-16 !mt-10 !container mx-auto"
+        className="!pb-10 !pl-5 md:!pl-10 lg:!pl-14 xl:!pl-16 !mt-10 !container mx-auto"
       >
-        {services.map((service: ServiceInterface, index: number) => (
+        {services.map((service, index) => (
           <SwiperSlide key={index}>
-            <div className=" p-5 flex flex-col gap-2 bg-white rounded-xl shadow-lg">
-              {service.imagem ? (
+            <div className="p-5 flex flex-col gap-2 bg-white rounded-xl shadow-lg">
+              {service.imagem && (
                 <img
                   src={service.imagem}
-                  alt={`Imagem de exemplo ${service.nome}`}
-                  className="rounded w-full  "
+                  alt={`Imagem de ${service.nome}`}
+                  className="rounded w-full"
                 />
-              ) : (
-                <span className="hidden"></span>
               )}
 
-              <h3 className="text-xl md:text-2xl font-semibold text-[#7c4f32] ">
+              <h3 className="text-xl md:text-2xl font-semibold text-[#7c4f32]">
                 {service.nome}
               </h3>
 
               <p>{service.resumo}</p>
 
               <button
-                className="mx-auto mt-2 px-5 py-2 rounded-xl text-lg uppercase transition-all bg-[#663210] text-white border-2 border-[#663210] hover:bg-white hover:text-[#663210] "
+                className="mx-auto mt-2 px-5 py-2 rounded-xl text-lg uppercase transition-all bg-[#663210] text-white border-2 border-[#663210] hover:bg-white hover:text-[#663210]"
                 onClick={() => openModal(service)}
               >
                 Saiba mais
@@ -77,28 +71,27 @@ export default function SlideServices({ services, light }: SlideServicesProps) {
             </div>
           </SwiperSlide>
         ))}
-
-        <div className="w-full flex justify-center">
-          <a
-            href="https://wa.me/5534998798280?text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20uma%20consulta%20com%20a%20Dra.%20Nara%20por%20favor."
-            target="_blank"
-            className={`
-                            w-fit mx-auto mt-10 px-5 py-2 text-lg font-semibold border-2 rounded-2xl 
-                            ${
-                              light
-                                ? "text-[#663210] hover:text-[#f0cca8] bg-[#f0cca8] hover:bg-[#663210] border-[#f0cca8] "
-                                : "text-white hover:text-[#663210] bg-[#663210] hover:bg-[#f0cca8] border-[#663210] "
-                            } 
-                        `}
-          >
-            Agendar consulta
-          </a>
-        </div>
       </Swiper>
+
+      <div className="w-full flex justify-center">
+        <a
+          href="https://wa.me/5534998798280?text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20uma%20consulta%20com%20a%20Dra.%20Nara%20por%20favor."
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`w-fit mx-auto mt-10 px-5 py-2 text-lg font-semibold border-2 rounded-2xl 
+            ${
+              light
+                ? "text-[#663210] hover:text-[#f0cca8] bg-[#f0cca8] hover:bg-[#663210] border-[#f0cca8]"
+                : "text-white hover:text-[#663210] bg-[#663210] hover:bg-[#f0cca8] border-[#663210]"
+            }`}
+        >
+          Agendar consulta
+        </a>
+      </div>
 
       {selectedService && (
         <ModalServices
-          isOpen={isModalOpen}
+          isOpen={!!selectedService}
           onClose={closeModal}
           service={selectedService}
         />
