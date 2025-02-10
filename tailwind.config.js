770acd997a61839bb7d2fe212ed6f6fdfe85263f
Beno <beno@MacBook-Pro-de-Beno.local>
2025-02-10 20:15:47 -0300

    teste no vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sentir-pele': "url('./assets/images/N-background.svg')",
+      },
+      backgroundSize: {
+        'custom-100': '100%',
+        'custom-80': '80%',
+        'custom-50': '50%',
+      },
+      backgroundPosition: {
+        'left-bottom': 'left bottom',
+        'right-bottom': 'right bottom',
+      },
+      colors: {
+        'sentir-pele': '#663210',
+      },
+    },
   },
   plugins: [],
 }
-
